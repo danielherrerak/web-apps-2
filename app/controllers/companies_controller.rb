c49cd3051103b455fff7efd79f7cf9645490f26a
danielherrerak <daniel.herrera@kellogg.northwestern.edu>
2022-05-12 03:57:31 +0000

    done!

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,27 @@
 class CompaniesController < ApplicationController
 
   # TODO:
-  # def index
-  # end
+  def index
+    @companies = Company.all
+  end
 
   def show
     @company = Company.find_by({ "id" => params["id"] })
+    @contacts = Contact.where (("compan_id" => @company["id"]))
+    # render
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new
+    @company["name"] = params["company"]["name"]
+    @company["city"] = params["company"]["city"]
+    @company["state"] = params["company"]["state"]
+    @company.save
+    redirect_to "/companies"
   end
 
 end
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,16 @@ class ContactsController < ApplicationController
 
   def new
     @contact = Contact.new
+    @compan_id["company_id"] = params ["company_id"]
   end
+
+def create
+  @contact = Contact.new
+  @contact["first_name"] = params["contact"]["first_name"]
+  @company["city"] = params["company"]["city"]
+  @company["state"] = params["company"]["state"]
+  @company.save
+  redirect_to "/companies"
+end
 
 end
